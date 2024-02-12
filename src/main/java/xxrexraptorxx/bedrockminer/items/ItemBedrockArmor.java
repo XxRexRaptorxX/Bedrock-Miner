d3aa6743d1e96b49fd5532690305f35f9d42e5ac
XxRexRaptorxX <Raptor-Simon@web.de>
2024-02-13 00:39:04 +0100

    armor effect changes

--- src/main/java/xxrexraptorxx/bedrockminer/items/ItemBedrockArmor.java
+++ src/main/java/xxrexraptorxx/bedrockminer/items/ItemBedrockArmor.java
@@ -1,11 +1,12 @@
 package xxrexraptorxx.bedrockminer.items;
 
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -13,7 +14,6 @@ import xxrexraptorxx.bedrockminer.registry.ModItems;
 import xxrexraptorxx.bedrockminer.utils.Config;
 
 public class ItemBedrockArmor extends ArmorItem {
-
 
     public ItemBedrockArmor(ArmorMaterial material, Type type, Properties properties) {
         super(material, type, properties);
@@ -25,20 +25,41 @@ public class ItemBedrockArmor extends ArmorItem {
 
     }
 
+
     @Override
-    public void onArmorTick(ItemStack stack, Level world, Player player) {
-        if (!world.isClientSide && Config.ARMOR_EFFECTS.get() && player.getInventory().getArmor(3) != ItemStack.EMPTY && player.getInventory().getArmor(3).getItem() == ModItems.BEDROCK_HELMET.get()
-                && player.getInventory().getArmor(2) != ItemStack.EMPTY && player.getInventory().getArmor(2).getItem() == ModItems.BEDROCK_CHESTPLATE.get()
-                && player.getInventory().getArmor(1) != ItemStack.EMPTY && player.getInventory().getArmor(1).getItem() == ModItems.BEDROCK_LEGGINGS.get()
-                && player.getInventory().getArmor(0) != ItemStack.EMPTY && player.getInventory().getArmor(0).getItem() == ModItems.BEDROCK_BOOTS.get()) {
-            this.effectPlayer(player, MobEffects.DAMAGE_RESISTANCE, 1);
-            this.effectPlayer(player, MobEffects.MOVEMENT_SLOWDOWN, 1);
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int p_41407_, boolean p_41408_) {
+        if (!level.isClientSide() && Config.ARMOR_EFFECTS.get() && entity instanceof Player) {
+            Player player = (Player) entity;
+
+            int armorCounter = 0;
+            Item helmet = player.getInventory().getArmor(3).getItem();
+            Item chestplate = player.getInventory().getArmor(2).getItem();
+            Item leggings = player.getInventory().getArmor(1).getItem();
+            Item boots = player.getInventory().getArmor(0).getItem();
+
+            if (helmet == ModItems.BEDROCK_HELMET.get()) armorCounter++;
+            if (chestplate == ModItems.BEDROCK_CHESTPLATE.get()) armorCounter++;
+            if (leggings == ModItems.BEDROCK_LEGGINGS.get()) armorCounter++;
+            if (boots == ModItems.BEDROCK_BOOTS.get()) armorCounter++;
+
+            switch (armorCounter) {
+                case 1:
+                    break;
+                case 2:
+                    player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30, 0, false, false, true));
+                    break;
+                case 3:
+                    player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30, 0, false, false, true));
+                    player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 30, 0, false, false, true));
+                    break;
+                case 4:
+                    player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30, 0, false, false, true));
+                    player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 30, 1, false, false, true));
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
-
-    private void effectPlayer(Player player, MobEffect effect, int amplifier) {
-        if (player.getEffect(effect) == null || player.getEffect(effect).getDuration() <= 70)
-            player.addEffect(new MobEffectInstance(effect, 70, amplifier, true, true));
-    }
 }
