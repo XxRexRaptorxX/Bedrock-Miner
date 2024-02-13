7865469b9796f9f039a8a3fc9886f7cf4cb24f2e
XxRexRaptorxX <Raptor-Simon@web.de>
2024-02-13 20:41:16 +0100

    bedrock armor effect changes

--- src/main/java/xxrexraptorxx/bedrockminer/items/ItemBedrockArmor.java
+++ src/main/java/xxrexraptorxx/bedrockminer/items/ItemBedrockArmor.java
@@ -6,6 +6,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -14,10 +15,10 @@ import xxrexraptorxx.bedrockminer.utils.Config;
 
 public class ItemBedrockArmor extends ArmorItem {
 
-
     public ItemBedrockArmor(ArmorMaterial material, Type type, Properties properties) {
         super(material, type, properties);
     }
+
 
     @Override
     public boolean isRepairable(ItemStack stack) {
@@ -25,14 +26,39 @@ public class ItemBedrockArmor extends ArmorItem {
 
     }
 
+
     @Override
-    public void onArmorTick(ItemStack stack, Level world, Player player) {
-        if (!world.isClientSide && Config.ARMOR_EFFECTS.get() && player.getInventory().getArmor(3) != ItemStack.EMPTY && player.getInventory().getArmor(3).getItem() == ModItems.BEDROCK_HELMET.get()
-                && player.getInventory().getArmor(2) != ItemStack.EMPTY && player.getInventory().getArmor(2).getItem() == ModItems.BEDROCK_CHESTPLATE.get()
-                && player.getInventory().getArmor(1) != ItemStack.EMPTY && player.getInventory().getArmor(1).getItem() == ModItems.BEDROCK_LEGGINGS.get()
-                && player.getInventory().getArmor(0) != ItemStack.EMPTY && player.getInventory().getArmor(0).getItem() == ModItems.BEDROCK_BOOTS.get()) {
-            this.effectPlayer(player, MobEffects.DAMAGE_RESISTANCE, 1);
-            this.effectPlayer(player, MobEffects.MOVEMENT_SLOWDOWN, 1);
+    public void onArmorTick(ItemStack stack, Level level, Player player) {
+        if (!level.isClientSide() && Config.ARMOR_EFFECTS.get()) {
+
+            int armorCounter = 0;
+            Item helmet = player.getInventory().getArmor(3).getItem();
+            Item chestplate = player.getInventory().getArmor(2).getItem();
+            Item leggings = player.getInventory().getArmor(1).getItem();
+            Item boots = player.getInventory().getArmor(0).getItem();
+
+            if (helmet == ModItems.BEDROCK_HELMET.get()) armorCounter++;
+            if (chestplate == ModItems.BEDROCK_CHESTPLATE.get()) armorCounter++;
+            if (leggings == ModItems.BEDROCK_LEGGINGS.get()) armorCounter++;
+            if (boots == ModItems.BEDROCK_BOOTS.get()) armorCounter++;
+
+            switch (armorCounter) {
+                case 1:
+                    break;
+                case 2:
+                    player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30, 0, false, false, true));
+                    break;
+                case 3:
+                    player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30, 0, false, false, true));
+                    player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 30, 0, false, false, true));
+                    break;
+                case 4:
+                    player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30, 0, false, false, true));
+                    player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 30, 1, false, false, true));
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
