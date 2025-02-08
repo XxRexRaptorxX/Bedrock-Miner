98c4cffbf02a8ede4f4a2d1733170af9fe490570
XxRexRaptorxX <Raptor-Simon@web.de>
2025-02-08 16:31:00 +0100

    upate to MC 1.21.4

--- src/main/java/xxrexraptorxx/bedrockminer/datagen/DataGenerators.java
+++ src/main/java/xxrexraptorxx/bedrockminer/datagen/DataGenerators.java
@@ -5,21 +5,23 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import xxrexraptorxx.bedrockminer.main.References;
 
 import java.util.concurrent.CompletableFuture;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = References.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
 
-
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
+    public static void gatherData(GatherDataEvent.Client event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-        BlockTagGen blockTags = new BlockTagGen(packOutput, lookupProvider, event.getExistingFileHelper());
 
-        generator.addProvider(event.includeServer(), blockTags);
+        BlockTagsProvider blocktags = new BlockTagGen(packOutput, lookupProvider);
+        generator.addProvider(true, blocktags);
+        generator.addProvider(true, new ModelDataGen(packOutput));
     }
 }
