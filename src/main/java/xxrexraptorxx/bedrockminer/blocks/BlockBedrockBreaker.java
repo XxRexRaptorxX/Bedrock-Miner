93c61a421a1d24115aa9261db7a7aea8445cd84d
XxRexRaptorxX <Raptor-Simon@web.de>
2025-02-22 19:15:44 +0100

    bedrock breaker improved

--- src/main/java/xxrexraptorxx/bedrockminer/blocks/BlockBedrockBreaker.java
+++ src/main/java/xxrexraptorxx/bedrockminer/blocks/BlockBedrockBreaker.java
@@ -8,7 +8,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
@@ -24,9 +24,11 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.redstone.Orientation;
-import net.neoforged.neoforge.common.Tags;
 import xxrexraptorxx.bedrockminer.registry.ModBlocks;
+import xxrexraptorxx.bedrockminer.registry.ModItems;
+import xxrexraptorxx.bedrockminer.registry.ModTags;
 import xxrexraptorxx.bedrockminer.utils.Config;
 
 import javax.annotation.Nullable;
@@ -89,26 +91,46 @@ public class BlockBedrockBreaker extends DirectionalBlock {
     public void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, @Nullable Orientation orientation, boolean movedByPiston) {
         if (!level.isClientSide) {
 
-            BlockPos harvestBlockPos = state.getValue(DirectionalBlock.FACING).equals(Direction.DOWN) ? pos.below() : pos.above();
-            BlockPos dropPos = state.getValue(DirectionalBlock.FACING).equals(Direction.DOWN) ? pos.above() : pos.below();
             boolean flag = state.getValue(POWERED);
 
+            //test if block is powered
             if (flag != level.hasNeighborSignal(pos)) {
+                //contract
                 if (flag) {
                     level.scheduleTick(pos, this, 4);
                     level.playSound((Player)null, pos, SoundEvents.PISTON_CONTRACT, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.15F + 0.F);
+                    level.gameEvent(GameEvent.BLOCK_DEACTIVATE, pos, GameEvent.Context.of(state));
 
                 } else {
+                    //extend
+                    BlockPos harvestBlockPos = state.getValue(DirectionalBlock.FACING).equals(Direction.DOWN) ? pos.below() : pos.above();
+                    BlockPos dropPos = state.getValue(DirectionalBlock.FACING).equals(Direction.DOWN) ? pos.above() : pos.below();
                     Block harvestblock = level.getBlockState(harvestBlockPos).getBlock();
-                    if (harvestblock == ModBlocks.FAKE_BEDROCK.get()) harvestblock = Blocks.BEDROCK;
 
                     level.playSound((Player)null, pos, SoundEvents.PISTON_EXTEND, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.15F + 0.F);
                     level.setBlock(pos, state.cycle(POWERED), 2);
+                    level.gameEvent(GameEvent.BLOCK_ACTIVATE, pos, GameEvent.Context.of(state));
 
+                    //test is block is valid
                     if (isValidBlock(harvestblock)) {
-                        level.playSound((Player)null, pos, SoundEvents.STONE_BREAK, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.15F + 0.F);
-                        ItemEntity item = new ItemEntity(level, (double)dropPos.getX() + 0.5F, (double)dropPos.getY(), (double)dropPos.getZ() + 0.5F, new ItemStack(harvestblock, 1));
-                        level.addFreshEntity(item);
+                        level.playSound(null, pos, SoundEvents.STONE_BREAK, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.15F);
+
+                        //test if block is vanilla bedrock [required because bedrock has no drops]
+                        if (harvestblock == Blocks.BEDROCK) {
+                            //destroy and drop the block [workaround]
+                            ItemEntity item = new ItemEntity(level, (double)dropPos.getX() + 0.5F, (double)dropPos.getY(), (double)dropPos.getZ() + 0.5F, new ItemStack(harvestblock, 1));
+                            level.addFreshEntity(item);
+
+                        //every other valid block
+                        } else {
+                            //get the block drops
+                            List<ItemStack> drops = Block.getDrops(harvestblock.defaultBlockState(), (ServerLevel) level, harvestBlockPos, null, null, new ItemStack(ModItems.BEDROCK_PICKAXE.get()));
+                            for (ItemStack drop : drops) {
+                                //spawn drops
+                                Block.popResource(level, dropPos, drop);
+                            }
+                        }
+                        //destroy the harvested block
                         level.destroyBlock(harvestBlockPos, false);
                         level.addDestroyBlockEffect(pos, harvestblock.defaultBlockState());
 
@@ -126,8 +148,7 @@ public class BlockBedrockBreaker extends DirectionalBlock {
             return (block == Blocks.BEDROCK || block == ModBlocks.FAKE_BEDROCK.get());
         }
 
-        return !(block == Blocks.COMMAND_BLOCK || block == Blocks.CHAIN_COMMAND_BLOCK || block == Blocks.REPEATING_COMMAND_BLOCK || block == Blocks.STRUCTURE_BLOCK ||
-                block == Blocks.STRUCTURE_VOID || block == Blocks.BARRIER || new ItemStack(block).is(ItemTags.LEAVES) || new ItemStack(block).is(ItemTags.SMALL_FLOWERS)  || new ItemStack(block).is(Tags.Items.CROPS));
+        return !(block.defaultBlockState().is(BlockTags.AIR) || block.defaultBlockState().is(ModTags.BEDROCK_BREAKER_IMMUNE));
     }
 
 
