c984b0419e956ad598d4b31330c4d7d5336c20e8
XxRexRaptorxX <Raptor-Simon@web.de>
2025-05-12 21:43:51 +0200

    items are now lava/fire resistance

--- src/main/java/xxrexraptorxx/bedrockminer/registry/ModItems.java
+++ src/main/java/xxrexraptorxx/bedrockminer/registry/ModItems.java
@@ -1,12 +1,15 @@
 package xxrexraptorxx.bedrockminer.registry;
 
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.component.DamageResistant;
 import net.minecraft.world.item.equipment.ArmorType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -26,16 +29,16 @@ public class ModItems {
 
     public static final DeferredItem<Item> BEDROCK_CHUNK = ITEMS.register("bedrock_chunk", () -> new Item(new Item.Properties().setId(itemId("bedrock_chunk"))));
 
-    public static final DeferredItem<Item> BEDROCK_SWORD = ITEMS.registerItem("bedrock_sword", props -> new Item(props.sword(ModTags.BEDROCK_TIER,  3, -2.4f)));
-    public static final DeferredItem<Item> BEDROCK_PICKAXE = ITEMS.registerItem("bedrock_pickaxe", props -> new Item(props.pickaxe(ModTags.BEDROCK_TIER,  1, -2.8f)));
-    public static final DeferredItem<AxeItem> BEDROCK_AXE = ITEMS.registerItem("bedrock_axe", props -> new AxeItem(ModTags.BEDROCK_TIER,  5, -3.0f, props));
-    public static final DeferredItem<ShovelItem> BEDROCK_SHOVEL = ITEMS.registerItem("bedrock_shovel", props -> new ShovelItem(ModTags.BEDROCK_TIER, 1.5f, -3.0f, props));
-    public static final DeferredItem<HoeItem> BEDROCK_HOE = ITEMS.registerItem("bedrock_hoe", props -> new HoeItem(ModTags.BEDROCK_TIER, -3.5f, -0.0f, props));
+    public static final DeferredItem<Item> BEDROCK_SWORD = ITEMS.registerItem("bedrock_sword", props -> new Item(props.sword(ModTags.BEDROCK_TIER,  3, -2.4f).component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
+    public static final DeferredItem<Item> BEDROCK_PICKAXE = ITEMS.registerItem("bedrock_pickaxe", props -> new Item(props.pickaxe(ModTags.BEDROCK_TIER,  1, -2.8f).component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
+    public static final DeferredItem<AxeItem> BEDROCK_AXE = ITEMS.registerItem("bedrock_axe", props -> new AxeItem(ModTags.BEDROCK_TIER,  5, -3.0f, props.component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
+    public static final DeferredItem<ShovelItem> BEDROCK_SHOVEL = ITEMS.registerItem("bedrock_shovel", props -> new ShovelItem(ModTags.BEDROCK_TIER, 1.5f, -3.0f, props.component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
+    public static final DeferredItem<HoeItem> BEDROCK_HOE = ITEMS.registerItem("bedrock_hoe", props -> new HoeItem(ModTags.BEDROCK_TIER, -3.5f, -0.0f, props.component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
 
-    public static final DeferredItem<ItemBedrockArmor> BEDROCK_HELMET = ITEMS.registerItem("bedrock_helmet", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.HELMET)));
-    public static final DeferredItem<ItemBedrockArmor> BEDROCK_CHESTPLATE = ITEMS.registerItem("bedrock_chestplate", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.CHESTPLATE)));
-    public static final DeferredItem<ItemBedrockArmor> BEDROCK_LEGGINGS = ITEMS.registerItem("bedrock_leggings", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.LEGGINGS)));
-    public static final DeferredItem<ItemBedrockArmor> BEDROCK_BOOTS = ITEMS.registerItem("bedrock_boots", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.BOOTS)));
+    public static final DeferredItem<ItemBedrockArmor> BEDROCK_HELMET = ITEMS.registerItem("bedrock_helmet", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.HELMET).component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
+    public static final DeferredItem<ItemBedrockArmor> BEDROCK_CHESTPLATE = ITEMS.registerItem("bedrock_chestplate", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.CHESTPLATE).component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
+    public static final DeferredItem<ItemBedrockArmor> BEDROCK_LEGGINGS = ITEMS.registerItem("bedrock_leggings", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.LEGGINGS).component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
+    public static final DeferredItem<ItemBedrockArmor> BEDROCK_BOOTS = ITEMS.registerItem("bedrock_boots", props -> new ItemBedrockArmor(props.humanoidArmor(ModArmorMaterials.BEDROCK_ARMOR_MATERIAL, ArmorType.BOOTS).component(DataComponents.DAMAGE_RESISTANT, new DamageResistant(DamageTypeTags.IS_FIRE))));
 
 
     public static ResourceKey<Item> itemId(String name) {
